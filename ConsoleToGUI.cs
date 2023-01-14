acbf9304b4baa91849002024effe2dfe01e2ba81
Utku Gülcü <utkugulcu@gmail.com>
2023-01-14 15:51:02 +0300

    Added the ability to change inputs
    Added alternative input to toggle. Added the ability to change inputs in editor. Fixed the default values

--- ConsoleToGUI.cs
+++ ConsoleToGUI.cs
@@ -2,25 +2,30 @@ using UnityEngine;
 
 public class ConsoleToGUI : MonoBehaviour
 {
+    [Header("Toggle Inputs")]
+    [SerializeField] KeyCode toggle = KeyCode.BackQuote;
+    [SerializeField] KeyCode toggleAlternative = KeyCode.DoubleQuote;
+    
+
     [Header("Text area properties")]
-    [SerializeField] [Range(0, 1920)] int textAreaXPosition = 10;
-    [SerializeField] [Range(0, 1080)] int textAreaYPosition = 370;
-    [SerializeField] [Range(0, 1920)] int textAreaWidth = 370;
-    [SerializeField] [Range(0, 1080)] int textAreaHeight = 370;
+    [SerializeField] [Range(0, 1920)] int textAreaXPosition = 15;
+    [SerializeField] [Range(0, 1080)] int textAreaYPosition = 272;
+    [SerializeField] [Range(0, 1920)] int textAreaWidth = 637;
+    [SerializeField] [Range(0, 1080)] int textAreaHeight = 194;
 
     [Header("Button properties")]
-    [SerializeField] [Range(0, 1920)] int buttonXPosition = 10;
-    [SerializeField] [Range(0, 1080)] int buttonYPosition = 370;
-    [SerializeField] [Range(0, 1920)] int buttonWidth = 370;
-    [SerializeField] [Range(0, 1080)] int buttonHeight = 370;
+    [SerializeField] [Range(0, 1920)] int buttonXPosition = 467;
+    [SerializeField] [Range(0, 1080)] int buttonYPosition = 72;
+    [SerializeField] [Range(0, 1920)] int buttonWidth = 185;
+    [SerializeField] [Range(0, 1080)] int buttonHeight = 38;
 
     string myLog = "*begin log";
     bool doShow;
     int kChars = 700;
-    
+
     void OnEnable() { Application.logMessageReceived += Log; }
     void OnDisable() { Application.logMessageReceived -= Log; }
-    void Update() { if (Input.GetKeyDown("`")) { doShow = !doShow; } }
+    void Update() { if (Input.GetKeyDown(toggle) || Input.GetKeyDown(toggleAlternative)) { doShow = !doShow; } }
     public void Log(string logString, string stackTrace, LogType type)
     {
         myLog = myLog + "\n" + logString;
@@ -30,7 +35,7 @@ public class ConsoleToGUI : MonoBehaviour
     void OnGUI()
     {
         if (!doShow) { return; }
-        
+
         GUI.TextArea(new Rect(textAreaXPosition, Screen.height - textAreaYPosition, textAreaWidth, textAreaHeight), myLog);
 
         if (GUI.Button(new Rect(buttonXPosition, Screen.height - buttonYPosition, buttonWidth, buttonHeight), "Clear"))
